20012356719967b210851df4d4372e0c5603dea6
pablog97 <pablojesus34@hotmail.com>
2023-10-20 02:01:47 -0600

    agregando vista para catalogo de métodos de pago

---
+++ src/main/java/com/kathsoft/kathpos/app/view/Fr_DatosFormaDePago.java
@@ -0,0 +1,43 @@
+package com.kathsoft.kathpos.app.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Fr_DatosFormaDePago extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	/*
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fr_DatosFormaDePago frame = new Fr_DatosFormaDePago();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public Fr_DatosFormaDePago(int opcion) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 360);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
