e99f80d385ac7f2b04b7e9b37aa44cfd5ccbd75c
pablo_gomez_perez <pablojesus34@hotmail.com>
2024-07-08 17:57:11 -0600

    Formato

--- src/main/java/com/kathsoft/kathpos/app/controller/CuentaContableController.java
+++ src/main/java/com/kathsoft/kathpos/app/controller/CuentaContableController.java
@@ -10,58 +10,56 @@ import com.kathsoft.kathpos.app.model.CuentaContable;
 import com.kathsoft.kathpos.tools.Conexion;
 
 public class CuentaContableController {
-	
+
 	private static Connection cn;
-	
+
 	public CuentaContableController() {
-		
+
 	}
-	
-	public Vector<Object[]> verCuentasContables(String cuenta){
+
+	public Vector<Object[]> verCuentasContables(String cuenta) {
 		var data = new Vector<Object[]>();
 		CallableStatement stm = null;
 		ResultSet rset = null;
-		
+
 		try {
-			
+
 			cn = Conexion.establecerConexionLocal(Conexion.DATA_BASE);
 			stm = cn.prepareCall("CALL ver_cuentas_contables(?)");
 			stm.setString(1, cuenta);
 			rset = stm.executeQuery();
-			
-			while(rset.next()) {
-				data.add(new Object[] {
-					rset.getInt(1), //id_cuenta
-					rset.getString(2), //Nombre de la cuenta contable superior
-					rset.getString(3), //clave de la cuenta contable
-					rset.getString(4), //Nombre cuenta
-					rset.getString(5), //descripcion
-					rset.getShort(6), // nivel
-					rset.getBoolean(7) ? "Si" : "No",
-					rset.getDouble(8), //cargos
-					rset.getDouble(9), //abonos
-					rset.getDouble(10) //saldo de la cuenta
+
+			while (rset.next()) {
+				data.add(new Object[] { rset.getInt(1), // id_cuenta
+						rset.getString(2), // Nombre de la cuenta contable superior
+						rset.getString(3), // clave de la cuenta contable
+						rset.getString(4), // Nombre cuenta
+						rset.getString(5), // descripcion
+						rset.getShort(6), // nivel
+						rset.getBoolean(7) ? "Si" : "No", rset.getDouble(8), // cargos
+						rset.getDouble(9), // abonos
+						rset.getDouble(10) // saldo de la cuenta
 				});
 			}
-			
+
 			return data;
-			
-		}catch(SQLException er) {
+
+		} catch (SQLException er) {
 			er.printStackTrace();
 			return null;
-		}catch(Exception er) {
+		} catch (Exception er) {
 			er.printStackTrace();
 			return null;
-		}finally {
+		} finally {
 			try {
 				Conexion.cerrarConexion(cn, rset, stm);
-			}catch(Exception er){
+			} catch (Exception er) {
 				er.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * @param clave -> Clave de la cuenta contable
@@ -69,34 +67,34 @@ public class CuentaContableController {
 	 */
 	public CuentaContable buscarCuentaPorClave(String clave) {
 		var data = new CuentaContable();
-		
-		try(var cn = Conexion.establecerConexionLocal(Conexion.DATA_BASE)){
-			
+
+		try (var cn = Conexion.establecerConexionLocal(Conexion.DATA_BASE)) {
+
 			var stm = cn.prepareCall("CALL buscar_cuenta_x_clave(?);");
 			stm.setString(1, clave);
 			var rset = stm.executeQuery();
-			
-			if(rset.next())
+
+			if (rset.next())
 				data.setIdCuenta(rset.getInt(1));
-				data.setIdCuentaPadre(rset.getInt(1));
-				data.setClave(rset.getString(3));
-				data.setNombre(rset.getString(4));
-				data.setDescripcion(rset.getString(5));
-				data.setNivel(rset.getShort(6));
-				data.setUltimoNivel(rset.getShort(7) == 1 ? true : false );
-				data.setNaturaleza(rset.getShort(8) == 1 ? true : false);
-				
+			data.setIdCuentaPadre(rset.getInt(1));
+			data.setClave(rset.getString(3));
+			data.setNombre(rset.getString(4));
+			data.setDescripcion(rset.getString(5));
+			data.setNivel(rset.getShort(6));
+			data.setUltimoNivel(rset.getShort(7) == 1 ? true : false);
+			data.setNaturaleza(rset.getShort(8) == 1 ? true : false);
+
 			return data;
-		}catch(SQLException er) {
+		} catch (SQLException er) {
 			er.printStackTrace();
 			return null;
-		}catch(Exception er) {
+		} catch (Exception er) {
 			er.printStackTrace();
 			return null;
 		}
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * @param idCuenta -> Indice de la cuenta contable
@@ -104,14 +102,14 @@ public class CuentaContableController {
 	 */
 	public CuentaContable buscarCuentaPorId(int idCuenta) {
 		var data = new CuentaContable();
-		
-		try(var cn = Conexion.establecerConexionLocal(Conexion.DATA_BASE)){
-			
+
+		try (var cn = Conexion.establecerConexionLocal(Conexion.DATA_BASE)) {
+
 			var stm = cn.prepareCall("CALL buscar_cuenta_x_id(?)");
 			stm.setInt(1, idCuenta);
 			var rset = stm.executeQuery();
-			
-			if(rset.next()) {
+
+			if (rset.next()) {
 				data.setIdCuenta(rset.getInt(1));
 				data.setNombreCuentaPadre(rset.getString(2));
 				data.setRubroCuenta(rset.getString(3));
@@ -124,16 +122,15 @@ public class CuentaContableController {
 				data.setSaldo(rset.getDouble(10));
 				data.setNaturaleza(rset.getShort(11) == 1 ? true : false);
 			}
-				
+
 			return data;
-		}catch(SQLException er) {
+		} catch (SQLException er) {
 			er.printStackTrace();
-			return null;			
-		}catch(Exception er) {
+			return null;
+		} catch (Exception er) {
 			er.printStackTrace();
 			return null;
 		}
 	}
-	
-	
+
 }
--- src/main/java/com/kathsoft/kathpos/app/view/contabilidad/PanelCuentasContables.java
+++ src/main/java/com/kathsoft/kathpos/app/view/contabilidad/PanelCuentasContables.java
@@ -29,7 +29,7 @@ public class PanelCuentasContables extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel panelEtiqueta;
-	private JTable tableCuentasContables;	
+	private JTable tableCuentasContables;
 	private DefaultTableModel tablaCuentasContablesModel;
 	private JPanel panelCentralSuperiorBotones;
 	private JButton btn_Agregar;
@@ -50,29 +50,29 @@ public class PanelCuentasContables extends JPanel {
 		setBorder(new EmptyBorder(0, 0, 0, 0));
 		setBackground(new Color(255, 215, 0));
 		setLayout(new BorderLayout(0, 0));
-		
+
 		panelEtiqueta = new JPanel();
 		panelEtiqueta.setBorder(new EmptyBorder(0, 0, 5, 0));
 		panelEtiqueta.setBackground(new Color(25, 25, 112));
 		add(panelEtiqueta, BorderLayout.NORTH);
-		
+
 		JLabel lblNewLabel = new JLabel("Contabilidad");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		panelEtiqueta.add(lblNewLabel);
-		
+
 		JPanel panelCentral = new JPanel();
 		panelCentral.setBorder(new EmptyBorder(30, 30, 30, 30));
 		panelCentral.setBackground(new Color(255, 215, 0));
 		add(panelCentral, BorderLayout.CENTER);
 		panelCentral.setLayout(new BorderLayout(0, 0));
-		
+
 		JScrollPane scrollPaneTablaConta = new JScrollPane();
 		panelCentral.add(scrollPaneTablaConta, BorderLayout.CENTER);
-		
+
 		this.tablaCuentasContablesModel = new DefaultTableModel();
-		
+
 		this.tablaCuentasContablesModel.addColumn("id");
 		this.tablaCuentasContablesModel.addColumn("Cta Superior");
 		this.tablaCuentasContablesModel.addColumn("Clave");
@@ -83,115 +83,121 @@ public class PanelCuentasContables extends JPanel {
 		this.tablaCuentasContablesModel.addColumn("Cargos");
 		this.tablaCuentasContablesModel.addColumn("Abonos");
 		this.tablaCuentasContablesModel.addColumn("Saldo");
-		
+
 		tableCuentasContables = new JTable();
 		tableCuentasContables.setModel(tablaCuentasContablesModel);
 		this.tableCuentasContables.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		scrollPaneTablaConta.setViewportView(tableCuentasContables);		
-		
+		scrollPaneTablaConta.setViewportView(tableCuentasContables);
+
 		panelCentralSuperiorBotones = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panelCentralSuperiorBotones.getLayout();
 		flowLayout.setAlignment(FlowLayout.RIGHT);
 		panelCentralSuperiorBotones.setBackground(new Color(255, 215, 0));
 		panelCentral.add(panelCentralSuperiorBotones, BorderLayout.NORTH);
-		
+
 		btn_Agregar = new JButton("Agregar");
 		btn_Agregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				abrirFormDatosCuentas(1,0);
-			}			
+				abrirFormDatosCuentas(1, 0);
+			}
 		});
-		btn_Agregar.setIcon(new ImageIcon(PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/agregar_ico.png")));
+		btn_Agregar.setIcon(new ImageIcon(
+				PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/agregar_ico.png")));
 		panelCentralSuperiorBotones.add(btn_Agregar);
-		this.btn_Agregar.setBackground(new Color(144,238,144));
-		
-		
+		this.btn_Agregar.setBackground(new Color(144, 238, 144));
+
 		btn_Modificar = new JButton("Modificar");
-		btn_Modificar.setIcon(new ImageIcon(PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/actualizar_ico.png")));
+		btn_Modificar.setIcon(new ImageIcon(
+				PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/actualizar_ico.png")));
 		panelCentralSuperiorBotones.add(btn_Modificar);
-		this.btn_Modificar.setBackground(new Color(144,238,144));
-		
+		this.btn_Modificar.setBackground(new Color(144, 238, 144));
+
 		btn_Eliminar = new JButton("Eliminar");
-		btn_Eliminar.setIcon(new ImageIcon(PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/nwCancel.png")));
+		btn_Eliminar.setIcon(new ImageIcon(
+				PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/nwCancel.png")));
 		panelCentralSuperiorBotones.add(btn_Eliminar);
-		this.btn_Eliminar.setBackground(new Color(255,51,0));
-		
+		this.btn_Eliminar.setBackground(new Color(255, 51, 0));
+
 		btn_ReporteExcel = new JButton("Exportar Excel");
-		btn_ReporteExcel.setIcon(new ImageIcon(PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/excelLogo.jpg")));
+		btn_ReporteExcel.setIcon(new ImageIcon(
+				PanelCuentasContables.class.getResource("/com/kathsoft/kathpos/app/assets/excelLogo.jpg")));
 		panelCentralSuperiorBotones.add(btn_ReporteExcel);
-		this.btn_ReporteExcel.setBackground(new Color(102,205,170));
-		
+		this.btn_ReporteExcel.setBackground(new Color(102, 205, 170));
+
 		panelInferiorBusqueda = new JPanel();
 		FlowLayout flowLayout_1 = (FlowLayout) panelInferiorBusqueda.getLayout();
 		flowLayout_1.setAlignment(FlowLayout.LEFT);
 		panelInferiorBusqueda.setBackground(new Color(255, 215, 0));
 		panelCentral.add(panelInferiorBusqueda, BorderLayout.SOUTH);
-		
+
 		lblNewLabel_1 = new JLabel("Cuenta Contable");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		panelInferiorBusqueda.add(lblNewLabel_1);
-		
+
 		horizontalStrut = Box.createHorizontalStrut(20);
 		panelInferiorBusqueda.add(horizontalStrut);
-		
+
 		txfBuscarCuentaContable = new JTextField();
 		panelInferiorBusqueda.add(txfBuscarCuentaContable);
 		txfBuscarCuentaContable.setColumns(70);
-		this.txfBuscarCuentaContable.setMaximumSize(this.txfBuscarCuentaContable.getPreferredSize());		
-		
+		this.txfBuscarCuentaContable.setMaximumSize(this.txfBuscarCuentaContable.getPreferredSize());
+
 		horizontalStrut_1 = Box.createHorizontalStrut(20);
 		panelInferiorBusqueda.add(horizontalStrut_1);
-		
+
 		btnBuscarCuenta = new JButton("Buscar");
 		btnBuscarCuenta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				llenarTablaCuentas();
 			}
 		});
-		this.btnBuscarCuenta.setBackground(new Color(184,134,11));
+		this.btnBuscarCuenta.setBackground(new Color(184, 134, 11));
 		panelInferiorBusqueda.add(btnBuscarCuenta);
-				
-		DataTools.definirTamanioDeColumnas(ConstantsConllections.tablaCuentasContablesColumnsWidth, tableCuentasContables);
-		
+
+		DataTools.definirTamanioDeColumnas(ConstantsConllections.tablaCuentasContablesColumnsWidth,
+				tableCuentasContables);
+
 	}
-	
+
 	public void llenarTablaCuentas() {
-		
+
 		var cuentas = AppContext.cuentaContableController.verCuentasContables(this.txfBuscarCuentaContable.getText());
-		
+
 		this.tablaCuentasContablesModel.getDataVector().removeAllElements();
 		this.tableCuentasContables.updateUI();
-		
+
 		cuentas.forEach(c -> {
 			this.tablaCuentasContablesModel.addRow(c);
 		});
-		
+
 	}
-	
+
 	/**
 	 * 
-	 * @param opcion -> indica el tipo de operación a relaizar {@code Update()} {@code Insert()}
-	 * @param idCuenta -> El id de la cuenta seleccionada a modificar, en su defecto es 0.
+	 * @param opcion   -> indica el tipo de operación a relaizar {@code Update()}
+	 *                 {@code Insert()}
+	 * @param idCuenta -> El id de la cuenta seleccionada a modificar, en su defecto
+	 *                 es 0.
 	 */
 	private void abrirFormDatosCuentas(int opcion, int idCuenta) {
-		
+
 		Component component = this;
-		
+
 		EventQueue.invokeLater(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				
+
 				try {
-					
+
 					Fr_DatosCuentasContables fr = new Fr_DatosCuentasContables(opcion, idCuenta);
 					fr.setLocationRelativeTo(component);
 					fr.setVisible(true);
-					
-				}catch(Exception er) {
+
+				} catch (Exception er) {
 					er.printStackTrace();
 				}
-								
+
 			}
 		});
 	}
