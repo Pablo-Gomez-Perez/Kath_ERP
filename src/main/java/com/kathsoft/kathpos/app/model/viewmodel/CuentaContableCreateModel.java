b50d0d06d66f10393b66cac921afd29c3b94f06c
PABLO JESÚS GÓMEZ PÉREZ <pablojesus34@hotmail.com>
2026-07-10 02:38:12 -0600

    Add account creation model

---
+++ src/main/java/com/kathsoft/kathpos/app/model/viewmodel/CuentaContableCreateModel.java
@@ -0,0 +1,12 @@
+package com.kathsoft.kathpos.app.model.viewmodel;
+
+/**
+ * Datos requeridos para registrar una cuenta contable.
+ *
+ * @param idCuentaPadre identificador de la cuenta superior
+ * @param idRubro identificador del rubro contable
+ * @param clave clave contable
+ * @param nombre nombre de la cuenta
+ * @param descripcion descripción de la cuenta
+ * @param ultimoNivel indica si la cuenta admite movimientos
+ */
