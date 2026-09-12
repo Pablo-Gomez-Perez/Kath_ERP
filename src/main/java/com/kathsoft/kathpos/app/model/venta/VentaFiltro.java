89de7843c2c61b7b77a1121866fdfb7b7a7fe737
Pablo-Gomez-Perez <pablojesus34@hotmail.com>
2026-09-12 13:49:42 -0600

    format: VentaFiltro

--- src/main/java/com/kathsoft/kathpos/app/model/venta/VentaFiltro.java
+++ src/main/java/com/kathsoft/kathpos/app/model/venta/VentaFiltro.java
@@ -5,57 +5,108 @@ import java.sql.Date;
 
 public class VentaFiltro implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String tipoBusqueda;
-    private String textoBusqueda;
-    private String ordenarPor;
-    private Date fechaInicial;
-    private Date fechaFinal;
+	private String tipoBusqueda;
+	private String textoBusqueda;
+	private String ordenarPor;
+	private Date fechaInicial;
+	private Date fechaFinal;
 
-    public VentaFiltro() {
-        super();
-    }
+	public VentaFiltro() {
+		super();
+	}
 
-    public VentaFiltro(String tipoBusqueda, String textoBusqueda, String ordenarPor, Date fechaInicial, Date fechaFinal) {
-        this.tipoBusqueda = tipoBusqueda;
-        this.textoBusqueda = textoBusqueda;
-        this.ordenarPor = ordenarPor;
-        this.fechaInicial = fechaInicial;
-        this.fechaFinal = fechaFinal;
-    }
+	public VentaFiltro(String tipoBusqueda, String textoBusqueda, String ordenarPor, Date fechaInicial,
+			Date fechaFinal) {
+		this.tipoBusqueda = tipoBusqueda;
+		this.textoBusqueda = textoBusqueda;
+		this.ordenarPor = ordenarPor;
+		this.fechaInicial = fechaInicial;
+		this.fechaFinal = fechaFinal;
+	}
 
-    public String getTipoBusqueda() { return tipoBusqueda; }
-    public void setTipoBusqueda(String tipoBusqueda) { this.tipoBusqueda = tipoBusqueda; }
-    public String getTextoBusqueda() { return textoBusqueda; }
-    public void setTextoBusqueda(String textoBusqueda) { this.textoBusqueda = textoBusqueda; }
-    public String getOrdenarPor() { return ordenarPor; }
-    public void setOrdenarPor(String ordenarPor) { this.ordenarPor = ordenarPor; }
-    public Date getFechaInicial() { return fechaInicial; }
-    public void setFechaInicial(Date fechaInicial) { this.fechaInicial = fechaInicial; }
-    public Date getFechaFinal() { return fechaFinal; }
-    public void setFechaFinal(Date fechaFinal) { this.fechaFinal = fechaFinal; }
+	public String getTipoBusqueda() {
+		return tipoBusqueda;
+	}
 
-    @Override
-    public String toString() {
-        return "VentaFiltro [tipoBusqueda=" + tipoBusqueda + ", textoBusqueda=" + textoBusqueda + ", ordenarPor="
-                + ordenarPor + ", fechaInicial=" + fechaInicial + ", fechaFinal=" + fechaFinal + "]";
-    }
+	public void setTipoBusqueda(String tipoBusqueda) {
+		this.tipoBusqueda = tipoBusqueda;
+	}
 
-    public static class VentaFiltroBuilder {
+	public String getTextoBusqueda() {
+		return textoBusqueda;
+	}
 
-        private final VentaFiltro filtro;
+	public void setTextoBusqueda(String textoBusqueda) {
+		this.textoBusqueda = textoBusqueda;
+	}
 
-        public VentaFiltroBuilder() {
-            this.filtro = new VentaFiltro();
-        }
+	public String getOrdenarPor() {
+		return ordenarPor;
+	}
 
-        public VentaFiltroBuilder tipoBusqueda(String value) { filtro.tipoBusqueda = value; return this; }
-        public VentaFiltroBuilder textoBusqueda(String value) { filtro.textoBusqueda = value; return this; }
-        public VentaFiltroBuilder ordenarPor(String value) { filtro.ordenarPor = value; return this; }
-        public VentaFiltroBuilder fechaInicial(Date value) { filtro.fechaInicial = value; return this; }
-        public VentaFiltroBuilder fechaFinal(Date value) { filtro.fechaFinal = value; return this; }
+	public void setOrdenarPor(String ordenarPor) {
+		this.ordenarPor = ordenarPor;
+	}
 
-        public VentaFiltro build() { return filtro; }
-    }
+	public Date getFechaInicial() {
+		return fechaInicial;
+	}
+
+	public void setFechaInicial(Date fechaInicial) {
+		this.fechaInicial = fechaInicial;
+	}
+
+	public Date getFechaFinal() {
+		return fechaFinal;
+	}
+
+	public void setFechaFinal(Date fechaFinal) {
+		this.fechaFinal = fechaFinal;
+	}
+
+	@Override
+	public String toString() {
+		return "VentaFiltro [tipoBusqueda=" + tipoBusqueda + ", textoBusqueda=" + textoBusqueda + ", ordenarPor="
+				+ ordenarPor + ", fechaInicial=" + fechaInicial + ", fechaFinal=" + fechaFinal + "]";
+	}
+
+	public static class VentaFiltroBuilder {
+
+		private final VentaFiltro filtro;
+
+		public VentaFiltroBuilder() {
+			this.filtro = new VentaFiltro();
+		}
+
+		public VentaFiltroBuilder tipoBusqueda(String value) {
+			filtro.tipoBusqueda = value;
+			return this;
+		}
+
+		public VentaFiltroBuilder textoBusqueda(String value) {
+			filtro.textoBusqueda = value;
+			return this;
+		}
+
+		public VentaFiltroBuilder ordenarPor(String value) {
+			filtro.ordenarPor = value;
+			return this;
+		}
+
+		public VentaFiltroBuilder fechaInicial(Date value) {
+			filtro.fechaInicial = value;
+			return this;
+		}
+
+		public VentaFiltroBuilder fechaFinal(Date value) {
+			filtro.fechaFinal = value;
+			return this;
+		}
+
+		public VentaFiltro build() {
+			return filtro;
+		}
+	}
 }
